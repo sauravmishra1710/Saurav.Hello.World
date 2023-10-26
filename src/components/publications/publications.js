f8d1d874420c9ce2e179f768544dbd0b51849097
Saurav Mishra <saurav.mishra@live.com>
2023-10-26 08:14:59 +0530

    add publications data

--- src/components/publications/publications.js
+++ src/components/publications/publications.js
@@ -3,51 +3,30 @@ import { MaterialReactTable } from 'material-react-table';
 import './publications.css'
 
 //nested data is ok, see accessorKeys in ColumnDef below
-const data = [
+const journals = [
   {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
+    slNo: 1,
+    title: 'A Comparative Study for Time-to-Event Analysis and Survival Prediction for Heart Failure Condition using Machine Learning Techniques.',
+    publishedOn: 'July 2022',
+    jName: <a href="https://jeeemi.org/index.php/jeeemi/issue/view/12" target="_blank">Journal of Electronics, Electromedical Engineering, and Medical Informatics Vol: 4, No: 3</a>,
+    linkToPdf: <a href="https://jeeemi.org/index.php/jeeemi/article/view/225/94" target="_blank">Read Paper Online</a>,
+    codeRepo: <a href="https://github.com/sauravmishra1710/Heart-Failure-Condition-And-Survival-Analysis" target="_blank">Source Code</a>,
   },
   {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
+    slNo: 2,
+    title: ' Artificial Intelligence: A Review of Progress and Prospects in Medicine and Healthcare.',
+    publishedOn: 'January 2022',
+    jName: <a href="https://jeeemi.org/index.php/jeeemi/issue/view/9" target="_blank">Journal of Electronics, Electromedical Engineering, and Medical Informatics Vol: 4, No: 1</a>,
+    linkToPdf: <a href="https://jeeemi.org/index.php/jeeemi/article/view/180/80" target="_blank">Read Paper Online</a>,
+    codeRepo: 'NA - This is a review paper.',
   },
   {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
+    slNo: 3,
+    title: 'A Comparative Study for Time-to-Event Analysis and Survival Prediction for Heart Failure Condition using Machine Learning Techniques.',
+    publishedOn: 'October 2021',
+    jName: <a href="https://jeeemi.org/index.php/jeeemi/issue/view/10" target="_blank">Journal of Electronics, Electromedical Engineering, and Medical Informatics Vol: 3, No: 3</a>,
+    linkToPdf: <a href="https://jeeemi.org/index.php/jeeemi/article/view/147/75" target="_blank">Read Paper Online</a>,
+    codeRepo: <a href="https://github.com/sauravmishra1710/Malaria-Detection-Using-Deep-Learning-Techniques" target="_blank">Source Code</a>,
   },
 ];
 
@@ -56,29 +35,34 @@ const Publications = () => {
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: 'slNo',
+        header: 'Sl No.',
         size: 150,
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
+        accessorKey: 'title',
+        header: 'Paper Title',
         size: 150,
       },
       {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: 'publishedOn',
+        header: 'Published On',
+        size: 150,
+      },
+      {
+        accessorKey: 'jName',
+        header: 'Journal Name',
         size: 200,
       },
       {
-        accessorKey: 'city',
-        header: 'City',
-        size: 150,
+        accessorKey: 'linkToPdf',
+        header: 'Paper Link',
+        size: 200,
       },
       {
-        accessorKey: 'state',
-        header: 'State',
-        size: 150,
+        accessorKey: 'codeRepo',
+        header: 'Code Repository',
+        size: 200,
       },
     ],
     [],
@@ -90,13 +74,19 @@ const Publications = () => {
         <h2> Peer Reviewed Publications </h2>
         <MaterialReactTable 
         columns={columns} 
-        data={data} 
+        data={journals} 
         enableSorting={false}
         enableFilters={false}
         enableDensityToggle={false}
         enableFullScreenToggle={false}
         enableColumnResizing={false}
-        enableClickToCopy/>
+        // enableClickToCopy
+        renderRowActionMenuItems={() => [<MenuItem>Test</MenuItem>]} muiTableBodyRowProps={{
+            onClick: () => {
+              alert('row click');
+            }
+          }}
+        />
     </div>
     </>
   );
