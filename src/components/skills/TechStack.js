a59a877425d255c28b92c7d6e5c947e5861695c7
Saurav Mishra <saurav.mishra@live.com>
2023-11-12 07:41:39 +0530

    realign the display of tech stack items

--- src/components/skills/TechStack.js
+++ src/components/skills/TechStack.js
@@ -15,6 +15,7 @@ import {
 } from "react-icons/io";
 
 import {
+  SiVisualbasic,
   SiDotnet,
   SiTensorflow,
   SiNumpy,
@@ -36,12 +37,23 @@ function Techstack() {
       <Row>
           <div className="skills-parent" id="skillsSection">
             <h2 id="skillHeading">Languages & Frameworks...</h2>
-            <div>
-              <div className="skills-card-container" style={{display: "flex", flexDirection: "row"}}>
+              <div className="all-skills-container">
                   <div className="skills-card">
                     <CardSkill
                       text="C#"
                       icon= {< TbBrandCSharp />}
+                    />      
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Visual Basic"
+                      icon= {< SiVisualbasic />}
+                    />      
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text=".Net"
+                      icon= {< SiDotnet />}
                     />      
                   </div>
                   <div className="skills-card">
@@ -52,19 +64,47 @@ function Techstack() {
                   </div>
                   <div className="skills-card">
                     <CardSkill
-                      text="Javascript"
-                      icon= {< DiJavascript1 />}
-                    />
-                  </div>
-                  
-                  <div className="skills-card">
-                    <CardSkill
                       text="React"
                       icon= {< DiReact />}
                     />
                   </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Javascript"
+                      icon= {< DiJavascript1 />}
+                    />
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Tensorflow"
+                      icon= {< SiTensorflow />}
+                    />
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Open CV"
+                      icon= {< SiOpencv />}
+                    />
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Numpy"
+                      icon= {< SiNumpy />}
+                    />
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Pandas"
+                      icon= {< SiPandas />}
+                    />
+                  </div>
+                  <div className="skills-card">
+                    <CardSkill
+                      text="Plotly"
+                      icon= {< SiPlotly />}
+                    />
+                  </div>
                 </div>
-            </div>
           </div>
       </Row>
     );
