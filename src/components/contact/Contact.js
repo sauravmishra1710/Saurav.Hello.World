60d84d1dd4fa1151099dbfeb308013f420be7b3d
Saurav Mishra <saurav.mishra@live.com>
2023-11-13 19:53:36 +0530

    Update contact page contents

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,8 +7,10 @@ import emailjs from "@emailjs/browser";
 import LoadingButton from "@mui/lab/LoadingButton";
 import SendIcon from "@mui/icons-material/Send";
 import ThemeContext from "../../utility/themeContext";
-import { Icon } from '@iconify/react';
-import contactLogo from '../../img/svg/connection.svg';
+import { Icon } from "@iconify/react";
+import img1 from "../../img/page_logos/connecting_people.jpg";
+import img2 from "../../img/page_logos/connection.svg";
+import envelope from "../../img/page_logos/envelope.png";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -83,64 +85,197 @@ function Contact() {
   }
 
   return (
-    
-        <div
-          id="contact-section"
-          style={{
-            // background: theme
-            //   ? "linear-gradient(to right,#232526,#414345)"
-            //   : "white",
-            // color: theme ? "white" : "black"
-          }}
-
-        >
-          <Row>
-            <Col md={6}>
-            <div className="contact-heading">
-              <h1 className="heading">Reach out to me...</h1>
-              <div className="connect-with-me-text">
-                <p>Thank you for visiting my space. Feel free to reach out, and let's embark on this journey of knowledge sharing, innovation, 
-                cullinary skills, lipsmacking food discoveries, cricket, football, and photography discoveries together. Always up to work on 
-                stuffs related to AI and Computer Vision in the healthcare domains. I am available in almost every social platform.
-                My Inbox is always open for all.</p>
-              </div>
-              <div>
-                <li className="social-icons">
+    <div
+      id="contact-section"
+      style={
+        {
+          // background: theme
+          //   ? "linear-gradient(to right,#232526,#414345)"
+          //   : "white",
+          // color: theme ? "white" : "black"
+        }
+      }
+    >
+      <Row>
+        <Col md={6}>
+          <div className="contact-heading">
+            <h1 className="heading">Reach out to me...</h1>
+            <div className="connect-with-me-text">
+              <p>
+                Thank you for visiting my space. Feel free to reach out, and
+                let's embark on this journey of knowledge sharing, innovation,
+                cullinary skills, lipsmacking food, cricket, football, and
+                photography discoveries together. Always up to work on stuffs
+                related to AI and Computer Vision in the healthcare domains. I
+                am available in almost every social platform. My Inbox is always
+                open for all.
+              </p>
+            </div>
+            <div>
+              <li className="social-icons">
+                <div className="social-icons-container">
                   <a
                     href="https://github.com/sauravmishra1710"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <Icon icon="logos:github-icon" className= "social_icons"/>
+                    <Icon icon="logos:github-icon" className="social_icons" />
                   </a>
+                  Github @{" "}
+                  <a
+                    href="https://github.com/sauravmishra1710"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    sauravmishra1710
+                  </a>
+                </div>
 
+                <div className="social-icons-container">
+                  <a
+                    href="https://scholar.google.com/citations?hl=en&user=t1GX3O8AAAAJ"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Icon
+                      icon="academicons:google-scholar-square"
+                      className="social_icons"
+                    />
+                  </a>
+                  Google Scholar @{" "}
+                  <a
+                    href="https://scholar.google.com/citations?hl=en&user=t1GX3O8AAAAJ"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Saurav Mishra
+                  </a>
+                </div>
+
+                <div className="social-icons-container">
+                  <a
+                    href="https://orcid.org/my-orcid?orcid=0000-0002-6107-0999"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Icon
+                      icon="academicons:orcid-square"
+                      className="social_icons"
+                    />
+                  </a>
+                  ORCID @{" "}
+                  <a
+                    href="https://orcid.org/my-orcid?orcid=0000-0002-6107-0999"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Saurav Mishra
+                  </a>
+                </div>
+
+                <div className="social-icons-container">
                   <a
                     href="https://www.linkedin.com/in/saurav-mishra1710/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <Icon icon="logos:linkedin-icon" className= "social_icons"/>
+                    <Icon icon="logos:linkedin-icon" className="social_icons" />
                   </a>
+                  Linkedin @{" "}
+                  <a
+                    href="https://www.linkedin.com/in/saurav-mishra1710/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    saurav-mishra1710
+                  </a>
+                </div>
 
+                <div className="social-icons-container">
                   <a
                     href="https://twitter.com/forevrManUtd"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <Icon icon="devicon:twitter" className= "social_icons"/>
+                    <Icon icon="devicon:twitter" className="social_icons" />
                   </a>
-                </li>
-              </div>
-            </div>
-          </Col>
+                  Tweet @{" "}
+                  <a
+                    href="https://twitter.com/forevrManUtd"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    forevrManUtd
+                  </a>
+                </div>
 
-          <Col md={5}>
-          <div class="col-lg-6 connect-image">
-            <img  class= "images" src={contactLogo} alt="" height={800} width={800} />
-        </div>
-          </Col>
-          </Row>
-          <Row>
+                <div className="social-icons-container">
+                  <a
+                    href="https://www.instagram.com/saurav.mishra1710/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Icon
+                      icon="skill-icons:instagram"
+                      className="social_icons"
+                    />
+                  </a>
+                  Instagram @{" "}
+                  <a
+                    href="https://www.instagram.com/saurav.mishra1710/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    saurav.mishra1710
+                  </a>
+                </div>
+
+                <div className="social-icons-container">
+                  <a
+                    href="https://www.instagram.com/flashyfilters.saurav1710/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Icon
+                      icon="skill-icons:instagram"
+                      className="social_icons"
+                    />
+                  </a>
+                  More Insta @{" "}
+                  <a
+                    href="https://www.instagram.com/flashyfilters.saurav1710/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    flashyfilters.saurav1710
+                  </a>
+                </div>
+              </li>
+            </div>
+          </div>
+        </Col>
+        <Col md={5}>
+          <div className="col-lg-6 connect-image">
+            <img
+              className="images"
+              src={img1}
+              alt=""
+              height={800}
+              width={800}
+            />
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={3}><img
+              className="images"
+              src={envelope}
+              alt=""
+              height={500}
+              width={500}
+            /></Col>
+        <Col md={7}>
           <div className="contact-row">
             <div className="contact">
               <div className="contact-card">
@@ -224,8 +359,14 @@ function Contact() {
               </div>
             </div>
           </div>
-          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <div>
+          <img src={img2} alt="" height={800} width={800} />
         </div>
+      </Row>
+    </div>
   );
 }
 
