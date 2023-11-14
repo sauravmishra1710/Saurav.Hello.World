2ce5243f0f03833f8b1cd12e54a7dda66e6cfed6
Saurav Mishra <saurav.mishra@live.com>
2023-11-14 12:55:59 +0530

    Redesign skill page contents
    Modify the tech stack and tool stack icons.

--- src/components/skills/ToolStack.js
+++ src/components/skills/ToolStack.js
@@ -2,48 +2,111 @@ import React, { useContext } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./skills.css";
 import CardSkill from "./cardSkill";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
+// import {
+//     DiChrome
+// } from "react-icons/di";
+
+// import {
+//     AiOutlineFileWord
+// } from "react-icons/ai";
+
+import { Icon } from "@iconify/react";
 
 function Toolstack() {
-    return (
-      <Row> 
-          <div
-            className="skills-parent"
-            id="skillsSection"
-          >
-            <h2 id="skillHeading">Process & Tools...</h2>
-            <div style={{display: "flex"}}>
-              <div className="skills-card">
-                
-                <CardSkill
-                  text="Github"
-                  icon= {< DiGit />}
-                />
-              </div>
-              <div className="skills-card">
-                <CardSkill
-                  text="Python"
-                  icon= {< DiPython />}
-                />      
-              </div>
-              <div className="skills-card">
-                <CardSkill
-                  text="Python"
-                  icon= {< DiReact />}
-                />      
-              </div>
-            </div>
+  return (
+    <Row>
+      <div className="skills-parent" id="skillsSection">
+        <h2 id="skillHeading">Process & Tools...</h2>
+        <div className="all-skills-container">
+        <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon-plain:visualstudio-wordmark" width="112" height="112" />
+              }
+            />
           </div>
-      </Row>
-    );
-  }
-  
-  export default Toolstack;
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:vscode-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:anaconda-wordmark" width="112" height="112" />
+              }
+            />
+          </div> 
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:jupyter-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:github-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="logos:jenkins" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="logos:new-relic" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:splunk-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:jira-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="academicons:mendeley-square" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="devicon:chrome-wordmark" width="112" height="112" />
+              }
+            />
+          </div>
+          <div className="skills-card">
+            <CardSkill
+              icon={
+                <Icon icon="vscode-icons:file-type-word" width="112" height="112" />
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </Row>
+  );
+}
+
+export default Toolstack;
--- src/components/skills/cardSkill.js
+++ src/components/skills/cardSkill.js
@@ -5,11 +5,11 @@ import "./cardSkill.css";
 const CardSkill = ({text, icon }) => {
   return (
     <div className="cardSkill-container">
-      <div style={{ marginTop: "20px" }}>
+      {/* <div style={{ position: "absolute" }}>
         <p style={{ color: "black", FontFace: "monospace" }}>{text}</p>
-      </div>
+      </div> */}
     
-      <IconContext.Provider value={{ size: "8em", color: "#7770F0", className: "skillIcons" }}>
+      <IconContext.Provider value={{ size: "8em", color: "#7770F0"}}>
         <div>
           {icon}
         </div>
